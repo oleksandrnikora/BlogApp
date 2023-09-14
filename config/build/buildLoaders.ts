18d2cf08c9297077210c85430452912e0c32a7ee
Oleksandr Nikora <mailnik@ukr.net>
2023-09-14 12:51:54 +0200

    add babel config and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,44 +2,62 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  };
 
-const fileLoader =   {
-  test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-  use: [
-    {
-      loader: 'file-loader',
+  const babelLoader = {
+    test: /\.(js|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
+            {
+              locales: ['en', 'ua'],
+              keyAsDefaultValue: false,
+              saveMissing: true,
+              outputPath: 'public/locales/{{locale}}/{{ns}}.json',
+            },
+          ],
+        ],
+      },
     },
-  ],
-};
-  
+  };
+
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack'],
-  }
+  };
 
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
       // Creates `style` nodes from JS strings
-      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
       // Translates CSS into CommonJS
       {
-        loader:"css-loader",
+        loader: 'css-loader',
         options: {
           modules: {
             auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-            localIdentName: isDev
-              ? "[path][name]__[local]--[hash:base64:5]"
-              :  "[hash:base64:8]"
-          },         
-        }
+            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
+          },
+        },
       },
       // Compiles Sass to CSS
-      "sass-loader",
+      'sass-loader',
     ],
   };
-
 
   const typescriptLoader = {
     test: /\.tsx?$/,
@@ -47,10 +65,5 @@ const fileLoader =   {
     exclude: /node_modules/,
   };
 
-  return [
-    fileLoader,
-    svgLoader,
-    typescriptLoader,
-    cssLoader,
-  ];
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
