1c4008f53eaf70c4d71d279657cab2848d2cf661
Oleksandr Nikora <mailnik@ukr.net>
2023-09-24 17:11:05 +0200

    add ErrorBoundary

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'warn',
         'no-underscore-dangle': 'off',
-        'max-len': ['error', { ignoreComments: true }],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
